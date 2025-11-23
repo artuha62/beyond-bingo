31683fc5414ca19385db6dedcf95db6088d7e2e2
Hellrain <hellrain.art@gmail.com>
2025-11-23 15:44:26 +0400

    minor fixes

---
+++ src/pages/Bingo/Bingo.jsx
@@ -0,0 +1,17 @@
+import Title from '../../components/Title/Title.jsx'
+import styles from './Bingo.module.css'
+import LogoutButton from '../../components/UI/LogoutButton.jsx'
+import Overlay from '../../components/Overlay/Overlay.jsx'
+import CardsGrid from '../../components/CardsGrid/CardsGrid.jsx'
+
+const Bingo = () => {
+  return (
+    <div className={styles.container}>
+      <Title />
+      <LogoutButton />
+      <Overlay />
+      <CardsGrid />
+    </div>
+  )
+}
+export default Bingo
